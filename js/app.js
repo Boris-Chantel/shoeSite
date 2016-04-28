9bb66fe49ddb03a79fb8c16dfe5023b3dadd479b
Boris-Chantel <you@example.com>
2016-04-28 17:43:57 -0400

    slider implemented

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log("test");
+var app = (function() {
+	var init = function () {
+		$('.Hero nav').find('button').on('click', Hero.changeSlide);
+	};
+
+	var Hero = {
+		changeSlide: function() {
+			var btn = $(this);
+
+			btn.addClass('active').siblings().removeClass('active');
+			$('.Hero__slides').find('.' + btn.attr('id')).addClass('active').siblings().removeClass('active');
+
+		}
+	};
+
+	return {
+		init: init
+	};
+})();
+
+app.init();
